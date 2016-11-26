4c04d9a2cdd92a74a528ccc2e3dc1994eebc407d
Diego_net <yakov-mamontov@yandex.ru>
2016-11-26 14:33:22 +0300

    Сигналы посылаются

--- roboWeb/src/main/java/rts/robot/web/controller/RobotController.java
+++ roboWeb/src/main/java/rts/robot/web/controller/RobotController.java
@@ -26,7 +26,8 @@ public class RobotController {
     public String forward() {
         LOGGER.info("Forward button was clicked");
         try {
-            serialPortManager.write(0x1);
+            byte c = 1;
+            serialPortManager.write(c);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,24 +37,48 @@ public class RobotController {
     @RequestMapping(value="/back", method = RequestMethod.POST)
     public String back() {
         LOGGER.info("Back button was clicked");
+        try {
+            byte c = 2;
+            serialPortManager.write(c);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "redirect:/home";
     }
 
     @RequestMapping(value="/left", method = RequestMethod.POST)
     public String left() {
         LOGGER.info("Left button was clicked");
+        try {
+            byte c = 4;
+            serialPortManager.write(c);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "redirect:/home";
     }
 
     @RequestMapping(value="/right", method = RequestMethod.POST)
     public String right() {
         LOGGER.info("Right button was clicked");
+        try {
+            byte c = 3;
+            serialPortManager.write(c);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "redirect:/home";
     }
 
     @RequestMapping(value="/stop", method = RequestMethod.POST)
     public String stop() {
         LOGGER.info("Stop button was clicked");
+        try {
+            byte c = 5;
+            serialPortManager.write(c);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "redirect:/home";
     }
 }
