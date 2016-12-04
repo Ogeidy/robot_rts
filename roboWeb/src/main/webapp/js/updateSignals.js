1667a3635280f6e3e64dcfe1a56ae0247cdaa012
Irina Bodnar <i.bodnar@geoscan.aero>
2016-12-04 19:26:54 +0300

    dfsa

--- roboWeb/src/main/webapp/js/updateSignals.js
+++ roboWeb/src/main/webapp/js/updateSignals.js
@@ -9,12 +9,13 @@ $(function(){
             cache : false,
             headers : { 'X-Ajax-call' : 'true'},
             success : function() {
-                console.log('bck: ', CURRENT_SIGNALS.bck);
-                console.log('bckRight: ', CURRENT_SIGNALS.bckRight);
-                console.log('bckLeft: ', CURRENT_SIGNALS.bckLeft);
-                console.log('fwd: ', CURRENT_SIGNALS.fwd);
-                console.log('fwdRight: ', CURRENT_SIGNALS.fwdRight);
-                console.log('fwdLeft: ', CURRENT_SIGNALS.fwdLeft);
+                var modelAttr = $("#modelAttr").val();
+                console.log('bck: ', modelAttr.bck);
+                console.log('bckRight: ', modelAttr.bckRight);
+                console.log('bckLeft: ', modelAttr.bckLeft);
+                console.log('fwd: ', modelAttr.fwd);
+                console.log('fwdRight: ', modelAttr.fwdRight);
+                console.log('fwdLeft: ', modelAttr.fwdLeft);
             },
             error : function() {
                 console.log('Error while calling ajax');
